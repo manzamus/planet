8afe46acab648fac20dd884e9b2777f25c6a8afe
mutoni <uwingeneyenazifa29@gmail.com>
2023-07-26 17:29:06 +0200

    planet

--- kan.js
+++ kan.js
@@ -4,3 +4,8 @@ let accaunt='owner'
 function logger(){
     console.log('my name is nazifa')
 }
+const calcAge2 =function(birthyear){
+    return 20370-birthyear;
+}
+const calaAge3=calcAge3=birthyear =>2037-birthyear
+const age3=calcAge(1991);

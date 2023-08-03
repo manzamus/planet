c93f8c41b1a78d2809ec0f8f2147807df0e8db82
mutoni <uwingeneyenazifa29@gmail.com>
2023-08-03 16:39:25 +0200

    planet

--- index.js
+++ index.js
@@ -1,14 +1,114 @@
-function taggle(){
-     let nav = document.querySelector('.nav-links')
-    if(nav.className === 'navlink'){
-        nav.classList +=" active"
-    }else{
-         nav.className = 'nav-link'
-     }
-}
-const friend1=('nazifa');
-const friend2=('FALI');
-const friend3=('nema');
+// function taggle(){
+//      let nav = document.querySelector('.nav-links')
+//     if(nav.className === 'navlink'){
+//         nav.classList +=" active"
+//     }else{
+//          nav.className = 'nav-link'
+//      }
+// }
+// const friend1=('nazifa');
+// const friend2=('FALI');
+// const friend3=('nema');
 
-const friends=('nazifa','FALI','nema');
-console.log(friends);
+// const friends=('nazifa','FALI','nema');
+
+
+// const name1='elle';
+// const name2='keza';
+// const name3='dada';
+
+// const names=['elle','keza','dada'];
+// console.log(names);
+
+// const years=new Array(1991,1984,2008,2020);
+// console.log(names[0]);
+// console.log(names[2]);
+
+// console.log(names.length);
+// console.log(names[names.length-1]);
+// names[2]='ketty';
+// console.log(names)
+
+
+// let js="amazing";
+// console.log(30+45-34);
+// console.log('nazifa');
+
+// let age=20;
+// age=21;
+
+// const birthday=2003
+// const now=2037
+// const agemama=naw-2000;
+// const agepapa=naw-2018;
+// console.log(agemama,agepapa/10,2==3);
+// let c=5;
+// let v=3;
+// let i=c+v
+
+
+// const car1="toyota";
+// const cac2="volvo";
+// const car3="bmw"
+// const cars=['toyota','volvo','bmw']
+// console.log(cars)
+
+
+// const fruits = ["Banana", "Orange", "Apple", "Mango"];
+// let fLen = fruits.length;
+
+// let text = "<ul>";
+// for (let i = 0; i < fLen; i++) {
+//   text += "<li>" + fruits[i] + "</li>";
+// }
+// text += "</ul>";
+
+// function sumation(num1,num2){
+// console.log(num1+num2)
+// }
+// function num1(four){
+//     let answer=six*2
+//     console.log(answer)
+//     return answer
+// } 
+
+// function fruitprocessor(apple,pranges){
+//     const juice='juice with ${apple} apple and $ {oranges} oranges.';
+//     return juice;
+// }
+
+// const applejuice=fruitprocessor(5, 0);
+// console.log(applejuice);
+
+// const appleOrangejuice=fruitprocessor(2, 4);
+// console.log(appleOrangejuice);
+// function calcAge(birthyeah){
+//     const age=2037-birthyeah;
+// }
+
+console.log(Boolean(0));
+console.log(Boolean(undefined));
+console.log(Boolean('nazifa'));
+console.log(Boolean({}));
+console.log(Boolean(''));
+
+
+// const money=100;
+// if(money){
+//     console.log("ntasohoke yose");
+//     else {
+//         console.log("turayakoresha");
+//     }
+// }
+
+const Array=["nazifa","cythia","bebe"]
+console.log(Array[0])
+console.log(Array[1])
+console.log(Array[2])
+
+for(let i=1;i<=100,i=i+1;){
+console.log(i)
+}
+
+
+

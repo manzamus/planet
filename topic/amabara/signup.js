5e7b6c4fdcfa7b0e7434c400a53cc8a60d21d84d
mutoni <uwingeneyenazifa29@gmail.com>
2023-08-09 16:36:27 +0200

    planet

--- topic/amabara/signup.js
+++ topic/amabara/signup.js
@@ -5,19 +5,18 @@ let form=document.getElementById("form")
 
 
 let nazifa=JSON.parse(localStorage.getItem("nazifa")) || []
-
 form.addEventListener("submit",(e)=>{
     e.preventDefault()
-
-let nazifas={
-    username:username.value,
-    email:email.value,
-    password:password.value
+    let nazifas={
+        username:username.Value,
+        email:email.value,
+        password:password.value
 }
-nazifa.find((dda)=>dda.email==email.value)
+nazifa.find((nany)=>nany.email==email.value)
 nazifa.push(nazifas)
-const ccc=JSON.stringify(nazifa)
-localStorage.setItem("nazifa",ccc)
-alert("byakoze")
-})
-    
+const dda=JSON.stringify(nazifas)
+localStorage.setItem("nazifas",dda)
+alert("byakunze")
+}
+)
+ 
